37b7490a0fc4b736bb0f266cc3154e81a9e36486
Trólka Kacper <97135299+Tkapcer@users.noreply.github.com>
2025-05-27 15:31:00 +0200

    Init i podstawowa obsługa kamery

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+# Zmienna z adresem RTSP kamery (np. Tapo C200)
+RTSP_URL = 'rtsp://testpscf:bajojajo@192.168.1.31:554/stream1'
+
+def generate_frames():
+    cap = cv2.VideoCapture(RTSP_URL)
+    if not cap.isOpened():
+        raise RuntimeError("Nie można połączyć się z kamerą")
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
